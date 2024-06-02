3b5c035d7ca14a1e20e998a153548aa3776427d3
accordarin <61953826+accordarin@users.noreply.github.com>
2024-06-01 20:34:35 -0700

    gp1: > 98%, gp2: > 95%
    Sample result from gp_checker2:
        Accuracy: 95.9047619047619
        Mistakes: straight (11), three of a kind (40), two pairs (35)
    
    New data format:
    [card, card, card, card, card, suit, suit, suit, suit, suit, num_unique_cards, num_unique_suits]

--- gp.py
+++ gp.py
@@ -1,73 +1,47 @@
 import numpy as np
 
 #%% PREPROCESSING
-'''def preprocess(data):
-    print(f"Before: {data[0]}")
-    new_data = np.empty([data.shape[0], 1], dtype=int)
-    for i in range(data.shape[0]):
-        hand = data[i]
-        # Sort in numerical order
-        hand.sort()
-        # Change format
-        #new_hand = np.full((20,), -1, dtype=int)
-        new_hand = np.zeros((20,), dtype=int)
-        for j in range(len(hand)):
-            if hand[j] < 13: # first suit
-                new_hand[4*j] = hand[j] + 1
-            elif hand[j] < 26: # second suit
-                new_hand[4*j + 1] = hand[j] % 13 + 1
-            elif hand[j] < 39: # third suit
-                new_hand[4*j + 2] = hand[j] % 13 + 1
-            else: # fourth suit
-                new_hand[4*j + 3] = hand[j] % 13 + 1
-        new_data[i] = np.linalg.norm(new_hand)
-    print(f"After: {new_data[0]}")
-    return new_data'''
-
-
 def preprocess(data):
-    #print(f"Before: {data[0]}")
+    print(f"Before: {data[0]}")
     
-    # Create new array to store data of new shape (20,)
-    new_data = np.empty([data.shape[0], 20], dtype=int)
+    # Create new array to store data of new shape (10,)
+    new_data = np.empty([data.shape[0], 12], dtype=int)
     
     
     for i in range(data.shape[0]):
         hand = data[i]
 
             
-        new_hand = np.zeros((5,4), dtype=int)
-          # 2d shape for sorting later
-        new_cards = np.zeros((5,), dtype=int)
-          # just the nonzero entries for sorting later
-          
+        new_hand = np.zeros((2,5), dtype=int)
+          # 2d shape for easier processing, sorting
         
         # Change format of each hand:
-            # * use number codes from first suit (except everything + 1
-            #   to differentiate from zero entries)
-            # * each card is represented by 4 numbers, the only nonzero
-            #   number's index corresponds to its suit
+            # new_hand[0]: hand using number codes from first suit
+            # new_hand[1]: number corresponding to each card's suit
         for j in range(len(hand)):
             if hand[j] < 13: # first suit
-                new_hand[j][0] = hand[j] + 1
-                new_cards[j] = hand[j] + 1
+                suit = 1
             elif hand[j] < 26: # second suit
-                new_hand[j][1] = hand[j] % 13 + 1
-                new_cards[j] = hand[j] % 13 + 1
+                suit = 2
             elif hand[j] < 39: # third suit
-                new_hand[j][2] = hand[j] % 13 + 1
-                new_cards[j] = hand[j] % 13 + 1
+                suit = 3
             else: # fourth suit
-                new_hand[j][3] = hand[j] % 13 + 1
-                new_cards[j] = hand[j] % 13 + 1
+                suit = 4
+            
+            new_hand[0, j] = hand[j] % 13
+            new_hand[1, j] = suit
         
+        # Sort by numerical order of card codes
+        order = np.argsort(new_hand[0])
+        new_hand = np.concatenate((new_hand[0][order], new_hand[1][order]))
         
-        # Sort in numerical order
-        order = new_cards.argsort()
-        new_hand = new_hand[order]
+        # Append number of unique cards, unique suits
+        unique_cards = np.unique(new_hand[:5]).shape[0]
+        unique_suits = np.unique(new_hand[5:]).shape[0]
+        new_hand = np.append(new_hand, [unique_cards, unique_suits])
         
-        # Change to 1d array to better match original data
+        # Resize to 1d array
         new_data[i] = new_hand.flatten()
         
-    #print(f"After: {new_data[0]}")
+    print(f"After: {new_data[0]}")
     return new_data
