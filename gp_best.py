d6c589aa3998318e6383288d76c17325d7bd13f7
accordarin <61953826+accordarin@users.noreply.github.com>
2024-06-02 18:51:27 -0700

    Update gp_best.py
    Still 100% accuracy but with only four numbers!

--- gp_best.py
+++ gp_best.py
@@ -1,16 +1,14 @@
 def preprocess(data):
-    #print(f"Before: {data[0]}")
     
     # Create new array to store data of new shape (10,)
-    new_data = np.empty([data.shape[0], 13], dtype=int)
+    new_data = np.empty([data.shape[0], 4], dtype=int)
     
-    
+    # For each poker hand in data
     for i in range(data.shape[0]):
         hand = data[i]
-
-            
+        
+        # Create 2d array for easier use 
         new_hand = np.zeros((2,5), dtype=int)
-          # 2d shape for easier processing, sorting
         
         # Change format of each hand:
             # new_hand[0]: hand using number codes from first suit
@@ -28,30 +26,17 @@ def preprocess(data):
             new_hand[0, j] = hand[j] % 13
             new_hand[1, j] = suit
         
-        # Sort in numerical order
-        order = np.argsort(new_hand[0])
-        new_hand = np.concatenate((new_hand[0][order], new_hand[1][order]))
-        
         # Record number of unique cards, unique suits
-        unique_cards = np.unique(new_hand[:5]).shape[0]
-        unique_suits = np.unique(new_hand[5:]).shape[0]
+        unique_cards = np.unique(new_hand[0]).shape[0]
+        unique_suits = np.unique(new_hand[1]).shape[0]
         
         # Record number of repeated cards, repeated suits
-        cards, counts = np.unique(new_hand[:5], return_counts=True)
+        cards, counts = np.unique(new_hand[0], return_counts=True)
         repeated_cards = cards[counts > 1].shape[0]
-        suits, counts = np.unique(new_hand[5:], return_counts=True)
+        suits, counts = np.unique(new_hand[1], return_counts=True)
         repeated_suits = suits[counts > 1].shape[0]
         
-        # Replace card codes with difference between each two subsequent codes
-        for k in range(len(hand) - 1):
-            new_hand[k] = new_hand[k+1] - new_hand[k]
-        new_hand = np.delete(new_hand, len(hand) - 1)
+        # Final data comes from these counts/ numbers
+        new_data[i] = np.array([unique_cards, unique_suits, repeated_cards, repeated_suits])
         
-        # Append number of unique cards, unique suits, repeated cards, repeated suits
-        new_hand = np.append(new_hand, [unique_cards, unique_suits, repeated_cards, repeated_suits])
-        
-        # Resize to 1d array
-        new_data[i] = new_hand.flatten()
-        
-    #print(f"After: {new_data[0]}")
     return new_data
